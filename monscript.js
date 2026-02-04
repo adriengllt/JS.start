49065d129343fc2e70c5b8a327aae0bd1a951214
adril <adril@MSI>
2026-02-04 11:41:11 +0100

    Tp5

--- monscript.js
+++ monscript.js
@@ -1,3 +1,31 @@
-alert(" Ceci est mon .JS ! ");
-let reponse = prompt('Quelle est ta série préférée ?');
-console.log(reponse);
+let numJour = 23;
+let nomJour = "Vendredi";
+let nomMois = "Novembre";
+let isHiver = false;
+ /**
+console.log(numJour);
+console.log(nomJour);
+console.log(nomMois);
+console.log(isHiver);**/
+
+
+let tabJour = ["Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi","Dimanche"];
+let tabMois = ["Janvier","Février","Mars","Avril","Mai","Juin","Juillet","Aout","Septembre","Octobre","Novembre","Décembre"];
+
+/*console.log(tabJour);
+console.log(nomMois);
+console.log(tabMois);*/
+
+let maDateV1 = {
+    "jour" : 21,
+    "mois" : "Juillet",
+}
+
+let maDateV2 = {
+    "jour" : 12,
+    "mois" : "Novembre",
+}
+
+
+console.log(maDateV1);
+console.log(maDateV2);

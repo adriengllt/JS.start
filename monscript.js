bfce2906e18a1f718d70a7ee85409a8f7ca89c41
adril <adril@MSI>
2026-02-04 11:59:59 +0100

    fin

--- monscript.js
+++ monscript.js
@@ -27,5 +27,29 @@ let maDateV2 = {
 }
 
 
-console.log(maDateV1);
-console.log(maDateV2);
+/*console.log(maDateV1);
+console.log(maDateV2); */
+
+let nbr1 = 3,nbr2 = 7,nbr3 = 8,nbr4 = 21;
+
+/*console.log(nbr1+nbr2);
+console.log(nbr4-nbr3);
+
+nbr2 = nbr2*nbr1;
+nbr2 += 1;
+
+console.log(nbr2);
+
+console.log(nbr4/nbr1);
+console.log(nbr4%xnbr1);*/
+
+let nomSerie;
+let nomPerso;
+
+nomSerie = prompt("Quelle est ta série préférée ?");
+nomPerso = prompt("Quelle est ton personnage favori ?");
+
+alert("Ma série préférée est "+nomSerie+" et mon personnage est "+nomPerso+"!")
+
+
+
